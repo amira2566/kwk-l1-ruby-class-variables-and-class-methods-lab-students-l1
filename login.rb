924f2fd9e660a75d228c994de94dddfdd8267c50
amira2566 <braemira2@icloud.com>
2018-06-08 20:36:24 +0000

    automatically backed up by learn

--- login.rb
+++ login.rb
@@ -1,13 +1,13 @@
-class User
+# class User
   
-  def name=(username)
-    @the_username = username
-  end
+#   def name=(username)
+#     @the_username = username
+#   end
   
-  def name
-    @the_username
-  end
-end
-jessica = User.new
-jessica.name="Jessica"
-puts jessica.name
+#   def name
+#     @the_username
+#   end
+# end
+# jessica = User.new
+# jessica.name="Jessica"
+# puts jessica.name
